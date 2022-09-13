7e9c7c00fd6db89c6000440492fccc46ad79352d
jsoldi <jsoldi@hotmail.com>
2022-09-14 00:30:20 +0200

    0.1.9: async

--- helium/client.ts
+++ helium/client.ts
@@ -1,30 +1,30 @@
 /// <reference path="./types.d.ts" />
 
 namespace Client {
-    export let timeout = 60000;
+    export let timeout = 120000;
 
-    export function get<E extends Endpoint>(port: string, name: E['name'], arg: E['in']) {
+    export async function get<E extends Endpoint>(port: string, name: E['name'], arg: E['in']) {
         if (arg == null || typeof arg !== "object" || Array.isArray(arg)) 
             throw new Error("Argument must be an object");   
     
         const query = Object.entries(arg).map(([key, value]) => `${key}=${encodeURIComponent((value as any ?? '').toString())}`).join('&');
-        const json = global.webRequest(`http://localhost:${port}/${name}?${query}`, 'GET', null, null, timeout, false).response as string;
-        return JSON.parse(json) as E['out'];
+        const result = await global.webRequestAsync(`http://localhost:${port}/${name}?${query}`, 'GET', null, null, timeout, false);
+        return JSON.parse(result.response as string) as E['out'];
     }
     
-    export function post<E extends Endpoint>(port: string, name: E['name'], arg: E['in']) {
+    export async function post<E extends Endpoint>(port: string, name: E['name'], arg: E['in']) {
         if (arg == null || typeof arg !== "object") 
             throw new Error("Argument must be an object or an array");   
 
-        const json = global.webRequest(
+        const result = await global.webRequestAsync(
             `http://localhost:${port}/${name}`, 
             'POST', 
             { 'Content-Type': 'application/json' }, 
             JSON.stringify(arg), 
             timeout, 
             false
-        ).response as string;
+        );
     
-        return JSON.parse(json) as E['out'];
+        return JSON.parse(result.response as string) as E['out'];
     }
 }
